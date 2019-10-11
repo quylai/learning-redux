84a57731c2770a7ba5746d07c01bc2527a06f2c4
quyl <quylai@gmail.com>
2019-10-11 17:35:15 -0500

    amending 05-finished

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -3,7 +3,7 @@ import React from "react";
 const PhotoGrid = React.createClass({
   render() {
     return(
-      <div className="single-photo">
+      <div className="photo-grid">
         I'm the photo grid
         
       </div>
