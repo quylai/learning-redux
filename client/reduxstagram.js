282d1730bdd55ab7512eef3d03f85d4864459377
quyl <quylai@gmail.com>
2019-10-11 17:23:47 -0500

    missing from 04

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -17,7 +17,7 @@ const router = (
   <Router history={browserHistory}>
     <Route path="/" component={Main}>
       <IndexRoute component={PhotoGrid}></IndexRoute>
-      <Route path="/view:postId" component={Single}></Route>
+      <Route path="/view/:postId" component={Single}></Route>
     </Route>
   </Router>
 
